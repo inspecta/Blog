ea69d8d126eefdcd27d55cd64b11f85d2e5d6f3b
Derrick Mulinde <djmulinde@gmail.com>
2023-01-20 17:44:58 +0300

    Add model associations and methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  validates :title, length: { in: 5..200 }
+  validates :text, length: { minimum: 5 }
+  validates :comments_counter, numericality: { only_integer: true }
+  validates :likes_counter, numericality: { only_integer: true }
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_many :posts, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+
+  validates :name, format: { with: /[a-zA-Z]/, message: 'Only letters allowed for name.' }
+  validates :photo, presence: true
+  validates :bio, length: { minimum: 5, too_short: 'Bio should be more than 20 characters' }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def most_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
