054c1a477ee731c85b39dc46f6fcb40110d4b67e
Derrick Mulinde <djmulinde@gmail.com>
2023-01-21 16:43:35 +0300

    Modify models to accomodate tests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,8 @@ class Comment < ApplicationRecord
 
   validates :text, presence: true
 
+  after_save :update_posts_counter
+
   def update_posts_counter
     post.increment!(:comments_counter)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,12 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  validates :title, length: { in: 5..200 }
+  validates :title, length: { maximum: 250 }, presence: true
   validates :text, length: { minimum: 5 }
-  validates :comments_counter, numericality: { only_integer: true }
-  validates :likes_counter, numericality: { only_integer: true }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  after_save :update_posts_counter
 
   def update_posts_counter
     author.increment!(:posts_counter)
