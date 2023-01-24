5effc24a1b4dba0cb1c41ee63b1285167696cd70
Derrick Mulinde <djmulinde@gmail.com>
2023-01-24 16:17:27 +0300

    Add posts request spec file

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,26 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:example) { get user_posts_path(1) }
+
+    it 'should return a successful response' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'shoud render posts/index.html.erb template' do
+      expect(response).to render_template('index')
+    end
+
+    it 'response body should contain' do
+      expect(response.body).to include('Posts Page')
+    end
+  end
+
+  describe 'GET /show' do
+    before(:example) { get user_posts_path(1,1)}
+
+    it 'should return a successful response' do
+      expect(response).to have_http_status(:success)
+    end
   end
 end
