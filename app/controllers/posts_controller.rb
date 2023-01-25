cfd7ae1dfd3dd002ddb85be9cf0bc5b9269087a5
Derrick Mulinde <djmulinde@gmail.com>
2023-01-25 23:17:28 +0300

    Update controller methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,18 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    user_id = params[:user_id]
+    @user = User.find(user_id)
+    @posts = @user.posts.order(created_at: :desc)
+  end
 
-  def show; end
+  def show
+    # URL parameters
+    user_id = params[:user_id]
+    post_id = params[:id]
+
+    @user = User.find(user_id)
+    @post = @user.posts.find(post_id)
+    @comments = @post.comments.order(created_at: :desc)
+    @likes = @post.likes.all
+  end
 end
