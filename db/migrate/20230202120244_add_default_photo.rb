f5a43a66cda9f38caf92dbacb47ff0c5c78e3de3
mule <belmeetmule@gmail.com>
2023-02-02 18:49:54 +0300

    Add default to photo attribute

--- db/migrate/20230202120244_add_default_photo.rb
+++ db/migrate/20230202120244_add_default_photo.rb
@@ -1,4 +1,5 @@
 class AddDefaultPhoto < ActiveRecord::Migration[7.0]
   def change
+    change_column_default :users, :photo, "https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?w=1480&t=st=1674661161~exp=1674661761~hmac=e529680000ea966150e5ea3b38241a0d8c9582faf23dac8af61ce1785cf27838"
   end
 end
