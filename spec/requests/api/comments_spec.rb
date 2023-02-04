781ce384315ae31f8cd35f68987018eca7e82fac
Derrick Mulinde <djmulinde@gmail.com>
2023-02-04 15:54:27 +0300

    Add generated spec folders

---
+++ spec/requests/api/comments_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::Comments', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/api/comments/index'
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/api/comments/show'
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
---
+++ spec/requests/api/posts_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::Posts', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/api/posts/index'
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/api/posts/show'
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
