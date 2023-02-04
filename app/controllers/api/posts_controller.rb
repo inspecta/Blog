428ff2d9578dc80797463784331c3681afd13688
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-02-04 14:29:41 +0100

    Add the index action to the posts controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,6 +2,8 @@ class Api::PostsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
+    @posts = User.find(params[:user_id]).posts
+    render json: @posts
   end
 
   def show; end
