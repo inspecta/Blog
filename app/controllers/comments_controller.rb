6492b57b85f141dab20dddf3aed1fb8d84c07bb3
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-02-04 11:24:22 +0100

    Update the comment controller with the destroy action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,7 +16,14 @@ class CommentsController < ApplicationController
     end
   end
 
-  # Delete a comment
+  def destroy
+    comment = Comment.find(params[:id])
+    if comment.destroy
+      redirect_to user_post_path, notice: 'Comment was successfully deleted.'
+    else
+      flash.now[:error] = 'Error: Couldnot delete comment'
+    end
+  end
 
   private
 
